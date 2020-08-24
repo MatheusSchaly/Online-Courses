1925076097d9ea63a6d3df2be86a859a16d4a762
Matheus Schaly <matheusmhs@hotmail.com>
2020-08-24 20:32:29 -0300

    3.5

--- Python_and_Flask_Bootcamp_Create_Websites_using_Flask/9_Flask_Large_Application/1_coding_along/app.py
+++
@@ -1,9 +0,0 @@
-from myproject import app
-from flask import render_template
-
-@app.route('/')
-def index():
-    return render_template('home.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
--- Python_and_Flask_Bootcamp_Create_Websites_using_Flask/9_Flask_Large_Application/1_coding_along/myproject/owners/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
-
-class AddForm(FlaskForm):
-
-    name = StringField('Name of Owner:')
-    pup_id = IntegerField("Id of Puppy: ")
-    submit = SubmitField('Add Owner')
